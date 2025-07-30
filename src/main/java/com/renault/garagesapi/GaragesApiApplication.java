f2d398f1f2018299fa4d66baa9efd9a80168eb25
Said EL INANI <said.elinani@aliasblue.com>
2025-07-30 21:46:12 +0100

    Initial commit

---
+++ src/main/java/com/renault/garagesapi/GaragesApiApplication.java
@@ -0,0 +1,13 @@
+package com.renault.garagesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaragesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/renault/garagesapi/GaragesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renault.garagesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaragesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
